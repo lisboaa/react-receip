cc389bccf9a67112d7f570d52fe69f1c919e1849
Douglas <douglas.lisboa@edu.unipar.br>
2019-08-27 15:34:08 -0300

    Adicionado texto  na view

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React,{useEffect, useState}from 'react';
+import Recipe from "./Recipe";
+import './App.css';
+
+const App = () => {
+
+  const APP_ID = "aa3112fa";
+  const APP_KEY = "a7643111dc3381ddf6a32dd41e263a5a";
+
+  const [recipes, setRecipes] = useState([]);
+
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() =>{
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    
+    const data = await response.json();
+    setRecipes(data.hits);
+  };
+
+  return(
+    <div className="App">
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type="submit">Search</button>
+      </form>
+      {recipes.map(recipe =>(
+        <Recipe/>
+      ))}
+    </div>
+  );
+};
+
+export default App;
